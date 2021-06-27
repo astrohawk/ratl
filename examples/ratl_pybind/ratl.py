b0e245d765d38f5fa42478bb83acec2126d9a321
Hamish Cook <hamish.cook@astrohawk.net>
2021-06-28 00:42:23 +1000

    remove need for Gist by doing spectrum analysis in python

--- examples/ratl_pybind/ratl.py
+++ examples/ratl_pybind/ratl.py
@@ -1,21 +1,41 @@
 import sys
 
 import matplotlib.pyplot as plt
+import numpy
 
 sys.path.append(sys.argv[1])
 import ratl_pybind
 
-if __name__ == "__main__":
-    x_axis = ratl_pybind.x_axis()
-    plt.plot(x_axis, ratl_pybind.reference_double_sine())
-    plt.plot(x_axis, ratl_pybind.reference_int_sine())
-    plt.plot(x_axis, ratl_pybind.test_int_sine())
-    plt.xlim(x_axis[0], x_axis[-1])
-    plt.ylim(top=0)
-    # plt.ylim(10**-14, 10**0)
-    # plt.yscale("log")
+
+def plot_pdfs():
+    plt.hist(ratl_pybind.triangular_pdf(), bins=100)
+    plt.hist(ratl_pybind.gaussian_pdf(), bins=100)
     plt.show()
 
-    # plt.hist(ratl_pybind.triangular_pdf(), bins=100)
-    # plt.hist(ratl_pybind.gaussian_pdf(), bins=100)
-    # plt.show()
+
+def plot_spectrums():
+    num_samples = ratl_pybind.num_samples()
+    num_freq = int(num_samples / 2)
+
+    def spectrum_db(audio_data):
+        assert (len(audio_data) == num_samples)
+        fft_out = numpy.fft.fft(audio_data * numpy.hanning(num_samples))
+        magnitude = numpy.abs(fft_out[0:num_freq])
+        magnitude_db = 20 * numpy.log10(magnitude / num_freq)
+        return magnitude_db
+
+    x_axis = numpy.linspace(0, int(ratl_pybind.samplerate() / 2), num_freq)
+    plt.plot(x_axis, spectrum_db(ratl_pybind.reference_double_sine()))
+    plt.plot(x_axis, spectrum_db(ratl_pybind.test_int_sine()))
+    plt.xlim(x_axis[0], x_axis[-1])
+    plt.ylim(top=0)
+    plt.show()
+
+
+def main():
+    # plot_pdfs()
+    plot_spectrums()
+
+
+if __name__ == "__main__":
+    main()
