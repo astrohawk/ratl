a082e6500b87228133c69917b8b6d75da9f8471e
Hamish Cook <hamish.cook@astrohawk.net>
2021-09-10 00:40:35 +1000

    remove const on convert type inputs

--- include/ratl/convert.hpp
+++ include/ratl/convert.hpp
@@ -34,9 +34,11 @@ template<
     class SampleConverter,
     typename OutputSampleType,
     typename InputSampleType>
-inline OutputSampleType convert_impl(const InputSampleType& sample, dither_generator& dither_gen) noexcept
+inline OutputSampleType convert_impl(const InputSampleType& sample) noexcept
 {
-    using sample_converter = SampleConverter<InputSampleType, OutputSampleType, dither_generator>;
+    using converter_dither_generator = detail::null_dither_generator;
+    using sample_converter = SampleConverter<InputSampleType, OutputSampleType, converter_dither_generator>;
+    converter_dither_generator dither_gen;
     return ratl::convert(sample, sample_converter(dither_gen));
 }
 
@@ -45,11 +47,9 @@ template<
     class SampleConverter,
     typename OutputSampleType,
     typename InputSampleType>
-inline OutputSampleType convert_impl(const InputSampleType& sample) noexcept
+inline OutputSampleType convert_impl(const InputSampleType& sample, dither_generator& dither_gen) noexcept
 {
-    using converter_dither_generator = detail::null_dither_generator;
-    using sample_converter = SampleConverter<InputSampleType, OutputSampleType, converter_dither_generator>;
-    converter_dither_generator dither_gen;
+    using sample_converter = SampleConverter<InputSampleType, OutputSampleType, dither_generator>;
     return ratl::convert(sample, sample_converter(dither_gen));
 }
 } // namespace detail
@@ -57,45 +57,63 @@ inline OutputSampleType convert_impl(const InputSampleType& sample) noexcept
 // convert
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType convert(const InputSampleType& sample, dither_generator& dither_gen) noexcept
+inline std::remove_cv_t<OutputSampleType> convert(const InputSampleType& sample) noexcept
 {
-    return detail::convert_impl<detail::default_sample_converter, OutputSampleType, InputSampleType>(
-        sample, dither_gen);
+    return detail::convert_impl<
+        detail::default_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample);
 }
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType convert(const InputSampleType& sample) noexcept
+inline std::remove_cv_t<OutputSampleType> convert(const InputSampleType& sample, dither_generator& dither_gen) noexcept
 {
-    return detail::convert_impl<detail::default_sample_converter, OutputSampleType, InputSampleType>(sample);
+    return detail::convert_impl<
+        detail::default_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample, dither_gen);
 }
 
 // reference_convert
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType reference_convert(const InputSampleType& sample, dither_generator& dither_gen) noexcept
+inline std::remove_cv_t<OutputSampleType> reference_convert(const InputSampleType& sample) noexcept
 {
-    return detail::convert_impl<detail::reference_sample_converter, OutputSampleType, InputSampleType>(
-        sample, dither_gen);
+    return detail::convert_impl<
+        detail::reference_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample);
 }
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType reference_convert(const InputSampleType& sample) noexcept
+inline std::remove_cv_t<OutputSampleType> reference_convert(
+    const InputSampleType& sample, dither_generator& dither_gen) noexcept
 {
-    return detail::convert_impl<detail::reference_sample_converter, OutputSampleType, InputSampleType>(sample);
+    return detail::convert_impl<
+        detail::reference_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample, dither_gen);
 }
 
 // fast_convert
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType fast_convert(const InputSampleType& sample, dither_generator& dither_gen) noexcept
+inline std::remove_cv_t<OutputSampleType> fast_convert(const InputSampleType& sample) noexcept
 {
-    return detail::convert_impl<detail::fast_sample_converter, OutputSampleType, InputSampleType>(sample, dither_gen);
+    return detail::convert_impl<
+        detail::fast_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample);
 }
 
 template<typename OutputSampleType, typename InputSampleType>
-inline OutputSampleType fast_convert(const InputSampleType& sample) noexcept
+inline std::remove_cv_t<OutputSampleType> fast_convert(
+    const InputSampleType& sample, dither_generator& dither_gen) noexcept
 {
-    return detail::convert_impl<detail::fast_sample_converter, OutputSampleType, InputSampleType>(sample);
+    return detail::convert_impl<
+        detail::fast_sample_converter,
+        std::remove_cv_t<OutputSampleType>,
+        std::remove_cv_t<InputSampleType>>(sample, dither_gen);
 }
 
 } // namespace ratl
